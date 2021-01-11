6d19c12e28943b69d806e064b513593d9db6295f
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-11 17:26:38 -0500

    Added script.js fiile & wrote js code to show/hide list heading if there are entries in the list

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,6 +1,15 @@
 $(() => {
-   
-   
-   
-    console.log("ready!");
+
+    //Toggle list headings views
+    if ($('#menu-list li').length === 0) {
+        $('#menu-heading').css('visibility', 'hidden');
+    }
+    if ($('#devoured-list li').length === 0) {
+        $('#devoured-heading').css('visibility', 'hidden');
+    }
+    
+    
+
+
+    
 });
