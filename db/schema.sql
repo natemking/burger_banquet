1029a58d5f6c9f5d27f17b966e1fa4e69f61333f
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-12 15:47:02 -0500

    Changed footer postion to fixed

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT 0,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
  );
 
